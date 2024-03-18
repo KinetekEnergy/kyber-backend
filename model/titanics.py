473b3588d50f3a1a3d7b1baa837c457f39c81d92
KinetekEnergy <transyt8@gmail.com>
2024-03-18 09:44:24 -0700

    more errors

--- model/titanics.py
+++ model/titanics.py
@@ -16,11 +16,12 @@ class TitanicRegression:
     def __init__(self):
         self.dt = None
         self.logreg = None
-        self.X_train = None
-        self.X_test = None
+        self.x_train = None
+        self.x_test = None
         self.y_train = None
         self.y_test = None
         self.encoder = None
+        self.initTitanic()  # Call initTitanic in the constructor
 
     def initTitanic(self):
         titanic_data = sns.load_dataset("titanic")
@@ -48,53 +49,69 @@ class TitanicRegression:
         # clean data
 
     def runDecisionTree(self):
-        X = self.td.drop("survived", axis=1)  # all except 'survived'
+        x = self.td.drop("survived", axis=1)  # all except 'survived'
         y = self.td["survived"]  # only 'survived'
-        self.X_train, X_test, self.y_train, y_test = train_test_split(
-            X, y, test_size=0.3, random_state=42
+        self.x_train, x_test, self.y_train, y_test = train_test_split(
+            x, y, test_size=0.3, random_state=42
         )
         dt = DecisionTreeClassifier()
-        dt.fit(self.X_train, self.y_train)
+        dt.fit(self.x_train, self.y_train)
         self.dt = dt
-        print(export_text(dt, feature_names=X.columns.tolist()))
+        print(export_text(dt, feature_names=x.columns.tolist()))
 
         # more code here
 
-    def runLogisticRegression(self):
+    def runLogisticRegression(self, x, y):
         # more code here
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            x, y, test_size=0.3, random_state=42
+        )
         self.logreg = LogisticRegression()
-        self.logreg.fit(self.X_train, self.y_train)
+        self.logreg.fit(self.x_train, self.y_train)
+
+    def predictSurvival(self, passenger):
+        new_passenger = {
+            "name": [passenger.get("name")],
+            "pclass": [passenger.get("pclass")],
+            "sex": [passenger.get("sex")],
+            "age": [passenger.get("age")],
+            "sibsp": [passenger.get("sibsp")],
+            "parch": [passenger.get("parch")],
+            "fare": [passenger.get("fare")],
+            "embarked": [passenger.get("embarked")],
+            "alone": [passenger.get("alone")],
+        }
+
+        # Preprocess the new passenger data
+        new_passenger["sex"] = 1 if new_passenger["sex"] == "male" else 0
+        new_passenger["alone"] = 1 if new_passenger["alone"] else 0
+
+        # Encode 'embarked' variable
+        embarked_value = new_passenger["embarked"]
+        embarked_encoded = self.enc.transform(np.array([embarked_value]).reshape(1, -1))
+
+        cols = ["embarked_" + val for val in self.enc.categories_[0]]
+        new_passenger.update(
+            {col: value for col, value in zip(cols, embarked_encoded.toarray()[0])}
+        )
+
+        # Drop unnecessary columns
+        new_passenger_df = pd.DataFrame(new_passenger)
+        new_passenger_df.drop(["name", "embarked"], axis=1, inplace=True)
+
+        aliveProb = self.logreg.predict_proba(self=self, x=new_passenger_df)[:, 1]
+
+        return aliveProb
 
 
 def initTitanic():
     global titanic_regression
     titanic_regression = TitanicRegression()
     titanic_regression.initTitanic()
-
+    x = titanic_regression.td.drop("survived", axis=1)
+    y = titanic_regression.td["survived"]
+    titanic_regression.runLogisticRegression(x, y)
 
 # From API
-def predictSurvival(self, passenger):
-    new_passenger = passenger.copy()
-    # Preprocess the new passenger data
-    new_passenger["sex"] = new_passenger["sex"].apply(lambda x: 1 if x == "male" else 0)
-    new_passenger["alone"] = new_passenger["alone"].apply(
-        lambda x: 1 if x == True else 0
-    )
-
-    # Encode 'embarked' variable
-    self.onehot = self.enc.transform(new_passenger[["embarked"]]).toarray()
-    cols = ["embarked_" + val for val in self.enc.categories_[0]]
-    new_passenger[cols] = pd.DataFrame(self.onehot, index=new_passenger.index)
-    new_passenger.drop(["name"], axis=1, inplace=True)
-    new_passenger.drop(["embarked"], axis=1, inplace=True)
-    aliveProb = np.squeeze(self.logreg.predict_proba(new_passenger))
-
-    # more code here
-    # interact with
-    return aliveProb
-
 
 # Sample usage without API
-if __name__ == "__main__":
-    # Initialize the Titanic model
-    initTitanic()
