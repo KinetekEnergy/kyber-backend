ba075ec215f4a1684c0fef297d716e2e820686a2
KinetekEnergy <transyt8@gmail.com>
2024-03-21 10:56:03 -0700

    checkpoint again

--- main.py
+++ main.py
@@ -20,16 +20,11 @@ from api.titanic import titanic_api
 from model.titanics import initTitanic
 
 # setup App pages
-from projects.projects import (
-    app_projects,
-)  # Blueprint directory import projects definition
-
 # Initialize the SQLAlchemy object to work with the Flask app instance
 db.init_app(app)
 
 # register URIs
 app.register_blueprint(titanic_api)
-app.register_blueprint(app_projects)  # register app pages
 
 
 @app.errorhandler(404)  # catch for URL not found
