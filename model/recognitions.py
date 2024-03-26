90da8c7527b96a14d7515d70aa478d0c5fa3754c
KinetekEnergy <transyt8@gmail.com>
2024-03-26 10:03:55 -0700

    remove from dict

--- model/recognitions.py
+++ model/recognitions.py
@@ -5,7 +5,9 @@ import cv2
 
 class DemographyRecognition:
     def recognize(self, base64_encoded):
-        img_path = base64.b64decode(base64_encoded) # decode image
+        parsed = base64_encoded["base64_encoded"]  # remove from dictionary
+        # print(parsed)
+        img_path = base64.b64decode(parsed)  # decode image
         attributes = ['age', 'gender', 'race'] # which attributes
 
         information = dp.analyze(img_path, attributes) # analyze
