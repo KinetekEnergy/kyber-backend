6138dd511c705f9e32d507469f499bb50cbd1de3
KinetekEnergy <transyt8@gmail.com>
2024-03-26 09:38:17 -0700

    recognition added

--- api/recognition.py
+++ api/recognition.py
@@ -1,23 +1,17 @@
 from flask import Blueprint, request, jsonify, current_app, Response
 from flask_restful import Api, Resource  # used for REST API building
 
-from model.titanics import TitanicRegression
+from model.recognitions import DemographyRecognition
 
 recognition_api = Blueprint("recognition_api", __name__, url_prefix="/api/titanic")
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(recognition_api)
 
-# Initialize the model
-titanic_model = TitanicRegression()
-
-
-class TitanicApi:
+class RecognitionApi:
     class _CRUD(Resource):
         def post(self):
             data = request.get_json()
-            print(data)
-            alive_prob = titanic_model.predictSurvival(data)
-            return jsonify(alive_prob)
-
+            demography = DemographyRecognition.recognize(data)
+            return jsonify(demography)
     api.add_resource(_CRUD, "/")
--- model/recognitions.py
+++ model/recognitions.py
@@ -1,5 +1,14 @@
-import deepface as dp
+import base64
+import json
+from deepface import DeepFace as dp
+import cv2
 
-objs = dp.analyze(
-    img_path="img4.jpg", actions=["age", "gender", "race"]
-)
+class DemographyRecognition:
+    def recognize(self, base64_encoded):
+        img_path = base64.b64decode(base64_encoded) # decode image
+        attributes = ['age', 'gender', 'race'] # which attributes
+
+        information = dp.analyze(img_path, attributes) # analyze
+
+        age_json = json.loads(information) # get the age
+        return (age_json) # return
