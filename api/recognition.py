f15b38d772218de916a0482580ab856ddf39f806
KinetekEnergy <transyt8@gmail.com>
2024-03-26 09:56:48 -0700

    added the link

--- api/recognition.py
+++ api/recognition.py
@@ -3,7 +3,7 @@ from flask_restful import Api, Resource  # used for REST API building
 
 from model.recognitions import DemographyRecognition
 
-recognition_api = Blueprint("recognition_api", __name__, url_prefix="/api/titanic")
+recognition_api = Blueprint("recognition_api", __name__, url_prefix="/api/recognition")
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(recognition_api)
@@ -12,6 +12,6 @@ class RecognitionApi:
     class _CRUD(Resource):
         def post(self):
             data = request.get_json()
-            demography = DemographyRecognition.recognize(data)
+            demography = DemographyRecognition.recognize(self, base64_encoded=data)
             return jsonify(demography)
     api.add_resource(_CRUD, "/")
