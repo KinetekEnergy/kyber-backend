4189ce8fd61f9857c8834db18e075b9859fed1fa
KinetekEnergy <transyt8@gmail.com>
2024-03-27 11:42:20 -0700

    formatting

--- model/recognitions.py
+++ model/recognitions.py
@@ -10,7 +10,7 @@ import numpy as np
 class DemographyRecognition:
     def recognize(self, base64_encoded):
         # image parsing in preparation
-        b64_string = base64_encoded #["base64_encoded"]  # remove from dictionary
+        b64_string = base64_encoded  # ["base64_encoded"]  # remove from dictionary
 
         if b64_string.startswith("data:image"):  # if headers --> remove
             b64_string = b64_string.split(",", 1)[1]
@@ -28,23 +28,23 @@ class DemographyRecognition:
         # -------------------------------------------------------------------------------- #
 
         # data extraction
-        data = information[0] # get the first element
-        age = data['age'] # age extraction
+        data = information[0]  # get the first element
+        age = data["age"]  # age extraction
 
-        bothGenders = data['gender'] # get both gender confidence rates
-        woman = bothGenders['Woman'] # woman confidence
-        man = bothGenders['Man'] # man confidence
-        
+        bothGenders = data["gender"]  # get both gender confidence rates
+        woman = bothGenders["Woman"]  # woman confidence
+        man = bothGenders["Man"]  # man confidence
+
         # based on the probabilities, find which is larger and return that
         gender = None
         if woman > man:
             gender = "Female"
         elif woman < man:
             gender = "Male"
-        
+
         # the order is very important (must be gender THEN age)
-        returnData = [gender, age] # create a list and send it
-        
+        returnData = [gender, age]  # create a list and send it
+
         return returnData  # return
 
     def parseImage():
